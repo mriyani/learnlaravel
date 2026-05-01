23ad1e21a8de0388819fc1117727349025c0c1d9
mriyani <ryaneq@gmail.com>
2026-05-01 11:59:11 +0800

    Implement layout page component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
-</head>
-
-<body>
-    <h1>About Us</h1>
-    <p>This is the about page.</p>
-</body>
-
-</html>
+<x-layout>
+    <h1>About Page</h1>
+    <p>Hello from the About Page.</p>
+</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us</title>
-</head>
-
-<body>
-    <h1>Contact Us</h1>
-    <p>This is the contact page.</p>
-</body>
-
-</html>
+<x-layout>
+    <h1>Contact Page</h1>
+    <p>Hello from the Contact Page.</p>
+</x-layout>
