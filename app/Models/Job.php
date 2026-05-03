fd25e6f79481747de434ebfdf25a0576a789add9
mriyani <ryaneq@gmail.com>
2026-05-03 13:39:00 +0800

    feat: introduce Eloquent ORM setup and demonstrate Tinker

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '$90,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Data Analyst',
-                'salary' => '$70,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Product Manager',
-                'salary' => '$100,000'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(Job::all(), fn($job) => $job['id'] === (int) $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $table = 'job_listings';
+    protected $fillable = [
+        'title',
+        'salary'
+    ];
 }
